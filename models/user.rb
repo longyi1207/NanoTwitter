591516d898b02a0005d8b6555f1cf3f3eda339e6
xzd1628 <xuzhendan1628@gmail.com>
2022-02-06 22:40:18 -0500

    ruby syntax change

--- models/user.rb
+++ models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     has_many :tweets
 
     has_many :user_followers
-    has_many :followers, :through => :user_followers
-    has_many :inverse_user_followers, :class_name => "UserFollower", :foreign_key => "follower_id"
-    has_many :inverse_followers, :through => :inverse_user_followers, :source => :user
+    has_many :followers, through: :user_followers
+    has_many :inverse_user_followers, class_name: :"UserFollower", foreign_key: :"follower_id"
+    has_many :inverse_followers, through: :inverse_user_followers, source: :user
 end
